a61adf4177922182def4178074e952f245e62572
steveliu22 <77029560+steveliu22@users.noreply.github.com>
2022-02-22 13:51:23 -0500

    added background + fixed navbar

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-const navigation_texts = document.querySelector('nav');
+var text = "Hi, welcome to my website! Let me introduce myself:";
+var textPosition = 0;
+var tracker = 0;
+var speed = 60;
+const getDoc = document.getElementById("about-me-header");
+function typewriter() {
+    
+    getDoc.innerHTML = "<strong>" + text.substring(0, textPosition) + "<span></span>" + "</strong>";
 
-const inner = navigation_texts.innerHTML;
+    if(tracker != text.length) {
+        textPosition += 1;
+        tracker += 1;
+        setTimeout(typewriter, speed);
+    }
 
-console.log(inner);
+    else {
+        setTimeout(erase, 6000);
+    }
+
+}
+
+function erase() {
+    
+    getDoc.innerHTML = "";
+
+    
+}
+
+window.addEventListener("load", typewriter);
+
+
+
